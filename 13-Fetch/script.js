273c96c4803c13a01adc53352b9b72ddbb0fe268
Pedro <doctorfast10.12@hotmail.com>
2024-06-13 16:48:16 -0300

    Fetch

---
+++ 13-Fetch/script.js
@@ -0,0 +1,21 @@
+const url = "https://api.thecatapi.com/v1/breeds"
+
+fetch(url)
+  .then(response => {
+    console.log(response)
+  }) .catch(error => console.log(error))
+
+
+const content = "Hello World"
+const Header = new Headers()
+
+Header.append("Content-Type", content)
+console.log(Header)
+console.log(Header.has("Content-Type"))
+Header.set("Content-Type", content.length)
+console.log(Header)
+
+const blob = new Blob()
+
+blob.text("string")
+console.log(blob.text())
