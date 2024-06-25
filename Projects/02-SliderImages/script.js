6b5eb080483fc2ca2f4c1ae4e52dc046b05185a0
Pedro <doctorfast10.12@hotmail.com>
2024-06-24 23:01:12 -0300

    slider

--- Projects/02-SliderImages/script.js
+++ Projects/02-SliderImages/script.js
@@ -1,3 +1,10 @@
 const nextButton = document.getElementById("next")
 const previousButton = document.getElementById("previous")
 
+previousButton.addEventListener("click", () => {
+  console.log("Oi")
+})
+
+nextButton.addEventListener("click", () => {
+  console.log("Oi")
+})
