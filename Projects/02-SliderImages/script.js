5b5e4bf2c3a17ecfd0cfbf8812ab4e4895a66326
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-06-25 14:56:59 -0300

    slider

--- Projects/02-SliderImages/script.js
+++ Projects/02-SliderImages/script.js
@@ -1,10 +1,29 @@
 const nextButton = document.getElementById("next")
 const previousButton = document.getElementById("previous")
+const imageContainer = document.getElementById("image--container")
+const images = imageContainer.querySelectorAll("li img")
+
+let currentIndex = 0
+
+const getCurrentSlide = (index) => {
+  if(index >= images.length){
+    currentIndex = 0
+  } else if(index < 0){
+    currentIndex = images.length - 1
+  } else {
+    currentIndex = index
+  }
+
+  const offSet = -currentIndex * 100
+  imageContainer.style.transform = `transform(${offSet}%)`
+}
 
 previousButton.addEventListener("click", () => {
-  console.log("Oi")
+  getCurrentSlide(currentIndex - 1)
 })
 
 nextButton.addEventListener("click", () => {
-  console.log("Oi")
+  getCurrentSlide(currentIndex + 1)
 })
+
+
