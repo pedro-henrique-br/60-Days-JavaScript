01516884a1802047fc8acaee2382dd78f3bc4e33
Pedro <doctorfast10.12@hotmail.com>
2024-06-27 16:15:49 -0300

    slider

--- Projects/02-SliderImages/script.js
+++ Projects/02-SliderImages/script.js
@@ -7,18 +7,30 @@ const nextButton = document.getElementById("next-button")
 const img = document.getElementById("img")
 
 let currentIndex = 1
+setInterval(() => {
+  index = 1
+  if(currentIndex >= index){
+    img.style.transition = "0.5s"
+    slideImg(currentIndex += 1)
+  }
+}, 4000)
 
 const slideImg = (index) => {
-  img.src = `./assets/img/img-${index}.jpg`
-  console.log(index)
-  if(index >= 7){
-    currentIndex = 0
-  } else if(index <= 0 || index <= -1){
-    currentIndex = 7
-    index = 0
-    img.src = `./assets/img/img-${currentIndex}.jpg`
-  }
+  img.style.opacity = 0
+  setTimeout(() => {
+    img.src = `./assets/img/img-${index}.jpg`
+    if(index >= 8 || currentIndex >= 8){
+      currentIndex = 1
+      img.src = `./assets/img/img-${currentIndex}.jpg`
+    } else if(index <= 0 || index <= -1){
+      currentIndex = 7
+      index = 0
+      img.src = `./assets/img/img-${currentIndex}.jpg`
+    }
+    img.style.opacity = 1
+  }, 200)
 }
+
 nextButton.addEventListener("click", () => {
   slideImg(currentIndex += 1)
 })
