2ce4c29392fd44b3dc80dee3210826f52e0d5a6f
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-06-27 14:41:07 -0300

    slider

--- Projects/02-SliderImages/script.js
+++ Projects/02-SliderImages/script.js
@@ -1,12 +1,29 @@
 const imageContainer = document.getElementById("image--container")
 const images = imageContainer.querySelectorAll("li img")
 
-let currentIndex = 0
+const previousButton = document.getElementById("previous-button")
+const nextButton = document.getElementById("next-button")
 
-const getCurrentSlide = (index) => {
+const img = document.getElementById("img")
+
+let currentIndex = 1
+
+const slideImg = (index) => {
+  img.src = `./assets/img/img-${index}.jpg`
   console.log(index)
-  console.log(imageContainer.querySelectorAll("img")[index])
-
+  if(index >= 7){
+    currentIndex = 0
+  } else if(index <= 0 || index <= -1){
+    currentIndex = 7
+    index = 0
+    img.src = `./assets/img/img-${currentIndex}.jpg`
+  }
 }
+nextButton.addEventListener("click", () => {
+  slideImg(currentIndex += 1)
+})
 
+previousButton.addEventListener("click", () => {
+  slideImg(currentIndex -= 1)
+})
 
